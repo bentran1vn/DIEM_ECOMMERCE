08ab7731607ecc039a330882ddcd0f73a0690fe1
bentran1vn <tan182205@gmail.com>
2025-04-25 12:36:35 +0700

    Match endpoint define

---
+++ DiemEcommerce.Contract/Services/Match/Queries.cs
@@ -0,0 +1,15 @@
+using DiemEcommerce.Contract.Abstractions.Messages;
+using DiemEcommerce.Contract.Abstractions.Shared;
+
+namespace DiemEcommerce.Contract.Services.Match;
+
+public class Queries
+{
+    public record GetAllMatchQuery(Guid? CategoryId, string? SearchTerm, int PageIndex, int PageSize)
+        : IQuery<PagedResult<Responses.MatchResponse>>;
+
+    public record GetMatchByIdQuery(Guid Id) : IQuery<Responses.MatchDetailResponse>;
+    
+    public record GetMatchByFactoryIdQuery(Guid Id) : IQuery<PagedResult<Responses.MatchResponse>>;
+
+}
---
+++ DiemEcommerce.Contract/Services/Match/Responses.cs
@@ -0,0 +1,29 @@
+namespace DiemEcommerce.Contract.Services.Match;
+
+public class Responses
+{
+    public class MatchResponse
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<string> CoverImages { get; set; }
+        public Guid CategoryId { get; set; } 
+        public string CategoryName { get; set; } 
+        public Guid FactoryId { get; set; }
+        public string FactoryName { get; set; }
+        public string FactoryAddress { get; set; }
+        public string FactoryPhoneNumber { get; set; }
+    }
+    
+    public class MatchDetailResponse : MatchResponse
+    {
+        public string FactoryEmail { get; set; }
+        public string FactoryWebsite { get; set; }
+        public string FactoryDescription { get; set; }
+        public string FactoryTaxCode { get; set; }
+        public string FactoryBankAccount { get; set; }
+        public string FactoryBankName { get; set; }
+        public string FactoryLogo { get; set; }
+        public ICollection<Feedback.Response.UserFeedbackResponse> Feedbacks { get; set; }
+    }
+}
