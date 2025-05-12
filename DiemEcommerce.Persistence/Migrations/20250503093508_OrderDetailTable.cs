78ebf0ce158ca3dc678d02aa1ca54806d3041a8c
bentran1vn <tan182205@gmail.com>
2025-05-12 16:08:45 +0700

    ExcluteMigration

--- DiemEcommerce.Persistence/Migrations/20250503093508_OrderDetailTable.cs
+++ DiemEcommerce.Persistence/Migrations/20250503093508_OrderDetailTable.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace DiemEcommerce.Persistence.Migrations
 {
+    [ExcludeFromCodeCoverage]
     /// <inheritdoc />
     public partial class OrderDetailTable : Migration
     {
